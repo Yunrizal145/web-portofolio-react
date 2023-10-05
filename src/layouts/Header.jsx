9526b63653f22840aa041d290aca9ba748181860
Yunrizal145 <hamad.yunrizal@gmail.com>
2023-10-05 23:39:23 +0700

    style: Menambahkan animasi pada header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,19 +1,28 @@
 import useScroll from "../utils/useScroll";
 import logoImage from "../assets/image/logo.png";
 import Navbar from "../components/Navbar";
+import { motion } from "framer-motion";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const Header = () => {
 	const scrolled = useScroll();
 
 	return (
-		<header
+		<motion.header
+			variants={staggerContainer}
+			whileInView="show"
+			initial="hidden"
+			transition={{ ease: "easeOut", duration: 2 }}
 			className={`w-full fixed z-20 ${
 				scrolled
 					? "bg-opacity-30 bg-gray-700 backdrop-blur-md h-16 -mt-3 lg:h-28"
 					: "bg-[#00095D]"
 			} transition-all duration-500`}
 		>
-			<div className="mx-auto">
+			<motion.div
+				variants={fadeIn("down", "tween", 0.3, 0.75)}
+				className="mx-auto"
+			>
 				<div className="flex items-center justify-between relative">
 					<div className="w-[150px] mx-6 pr-10 py-8 lg:py-10 lg:w-[450px] lg:px-[70px]">
 						<a href="#" className="block">
@@ -24,8 +33,8 @@ const Header = () => {
 						<Navbar />
 					</div>
 				</div>
-			</div>
-		</header>
+			</motion.div>
+		</motion.header>
 	);
 };
 

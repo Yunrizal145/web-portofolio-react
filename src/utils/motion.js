3f9ea6590b937a0928b6880fbe2874b46de32d5c
Yunrizal145 <hamad.yunrizal@gmail.com>
2023-10-05 23:31:20 +0700

    edit: menambahkan data pada motion

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -9,3 +9,25 @@ export function staggerContainer(staggerChildren, delayChildren) {
         },
     };
 }
+
+export function fadeIn(direction, type, delay, duration) {
+    return {
+        hidden: {
+            x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+            y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+            opacity: 0,
+        },
+        show: {
+            x: 0,
+            y: 0,
+            opacity: 1,
+            transition: {
+
+            type,
+            delay,
+            duration,
+            ease: "easeOut",
+            },
+        },
+    };
+}

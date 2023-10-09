c5ffc7d39a4b272fdcde8a1b6105b95f1746d720
Yunrizal145 <hamad.yunrizal@gmail.com>
2023-10-09 22:54:26 +0700

    style: Menambahkan animasi pada halaman Portofolio

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -1,6 +1,12 @@
 import SectionTitle from "../components/SectionTitle";
 import img1 from "../assets/image/sertif-kawah.png";
 import { motion } from "framer-motion";
+import {
+	slideIn,
+	staggerContainer,
+	textVariants,
+	zoomIn,
+} from "../utils/motion";
 
 const Portfolio = () => {
 	console.log();
@@ -21,35 +27,69 @@ const Portfolio = () => {
 			<div className="mx-10 mt-14 md:mx-20">
 				<div className="w-full lg:w-[1250px] lg:mx-auto">
 					{/* Title of Portfolio */}
-					<div className="w-56 lg:w-[550px]">
-						<motion.h2 className="text-xl lg:text-4xl text-white font-bold">
+					<motion.div
+						animate={{ y: [-350, 0] }}
+						viewport={{ once: true, amount: 0.25 }}
+						transition={{ ease: "easeOut", duration: 2 }}
+						variants={staggerContainer}
+						whileInView="show"
+						initial="hidden"
+						className="w-56 lg:w-[550px]"
+					>
+						<motion.h2
+							variants={textVariants(1.1)}
+							className="text-xl lg:text-4xl text-white font-bold"
+						>
 							Completed Projects 👨‍💻
 						</motion.h2>
-						<h4 className="text-sm mt-4 text-slate-300 lg:text-lg lg:mt-6">
+						<motion.h4
+							variants={textVariants(1.2)}
+							className="text-sm mt-4 text-slate-300 lg:text-lg lg:mt-6"
+						>
 							Is a collection of achievements and works that
 							depict my journey, reflecting abilities and
 							dedication in creative endeavors.
-						</h4>
-					</div>
+						</motion.h4>
+					</motion.div>
 
-					<div className="mt-8 w-full">
+					<motion.div
+						animate={{ y: [-350, 0] }}
+						viewport={{ once: true, amount: 0.25 }}
+						transition={{ ease: "easeOut", duration: 2 }}
+						variants={staggerContainer}
+						whileInView="show"
+						initial="hidden"
+						className="mt-8 w-full"
+					>
 						{/* Main Content Portfolio Left */}
-						<div className="flex flex-col lg:flex-row lg:justify-between bg-blue-500 rounded-2xl mb-10">
+						<motion.div
+							variants={slideIn("bottom", "tween", 2, 1)}
+							className="flex flex-col lg:flex-row lg:justify-between bg-blue-500 rounded-2xl mb-10"
+						>
 							{/* IMG Portfolio */}
-							<div className="lg:w-1/2 flex justify-center m-10 rounded-2xl overflow-hidden">
+							<motion.div
+								variants={zoomIn(1.5, 0.75)}
+								className="lg:w-1/2 flex justify-center m-10 rounded-2xl overflow-hidden"
+							>
 								<img
 									src={img1}
 									alt="Sertif"
 									className="w-[432px]"
 								/>
-							</div>
+							</motion.div>
 
 							{/* Desc Portfolio */}
 							<div className="lg:w-1/2 lg:mt-8 mx-10 mb-10 text-white">
-								<h2 className="text-2xl mb-6 lg:text-4xl">
+								<motion.h2
+									variants={textVariants(1.1)}
+									className="text-2xl mb-6 lg:text-4xl"
+								>
 									Sertifikat
-								</h2>
-								<h3 className="text-sm mb-8 lg:text-lg">
+								</motion.h2>
+								<motion.h3
+									variants={textVariants(1.2)}
+									className="text-sm mb-8 lg:text-lg"
+								>
 									`A written recognition that states the
 									achievement, competence, or participation in
 									a program, course, or activity according to
@@ -58,26 +98,44 @@ const Portfolio = () => {
 									competence, or participation in a program,
 									course, or activity according to
 									predetermined criteria. `
-								</h3>
-								<div className="w-44 px-6 py-3 bg-[#ff4495] mb-4 rounded-2xl">
+								</motion.h3>
+								<motion.div
+									variants={textVariants(1.3)}
+									className="w-44 px-6 py-3 bg-[#ff4495] mb-4 rounded-2xl"
+								>
 									Tech Stack In Use
-								</div>
-								<div className="mt-8">
-									<button className="w-full px-6 py-2 bg-[#00095d] text-center rounded-3xl">
+								</motion.div>
+								<motion.div
+									variants={zoomIn(1.5, 0.75)}
+									className="mt-8"
+								>
+									<motion.button
+										variants={textVariants(1.4)}
+										className="w-full px-6 py-2 bg-[#00095d] text-center rounded-3xl"
+									>
 										See Details
-									</button>
-								</div>
+									</motion.button>
+								</motion.div>
 							</div>
-						</div>
+						</motion.div>
 
 						{/* Main Content Portfolio Right */}
-						<div className="flex flex-col-reverse lg:flex-row lg:justify-between bg-emerald-500 rounded-2xl ">
+						<motion.div
+							variants={slideIn("bottom", "tween", 2, 1)}
+							className="flex flex-col-reverse lg:flex-row lg:justify-between bg-emerald-500 rounded-2xl "
+						>
 							{/* Desc Portfolio */}
 							<div className="lg:w-1/2 lg:mt-8 mx-10 mb-10 text-white">
-								<h2 className="text-2xl mb-6 lg:text-4xl">
+								<motion.h2
+									variants={textVariants(1.1)}
+									className="text-2xl mb-6 lg:text-4xl"
+								>
 									Sertifikat
-								</h2>
-								<h3 className="text-sm mb-8 lg:text-lg">
+								</motion.h2>
+								<motion.h3
+									variants={textVariants(1.2)}
+									className="text-sm mb-8 lg:text-lg"
+								>
 									`A written recognition that states the
 									achievement, competence, or participation in
 									a program, course, or activity according to
@@ -86,27 +144,39 @@ const Portfolio = () => {
 									competence, or participation in a program,
 									course, or activity according to
 									predetermined criteria. `
-								</h3>
-								<div className="w-44 px-6 py-3 bg-[#ff4495] mb-4 rounded-2xl">
+								</motion.h3>
+								<motion.div
+									variants={textVariants(1.3)}
+									className="w-44 px-6 py-3 bg-[#ff4495] mb-4 rounded-2xl"
+								>
 									Tech Stack In Use
-								</div>
-								<div className="mt-8">
-									<button className="w-full px-6 py-2 bg-[#00095d] text-center rounded-3xl">
+								</motion.div>
+								<motion.div
+									variants={zoomIn(1.5, 0.75)}
+									className="mt-8"
+								>
+									<motion.button
+										variants={textVariants(1.4)}
+										className="w-full px-6 py-2 bg-[#00095d] text-center rounded-3xl"
+									>
 										See Details
-									</button>
-								</div>
+									</motion.button>
+								</motion.div>
 							</div>
 
 							{/* IMG Portfolio */}
-							<div className="lg:w-1/2 flex justify-center m-10 rounded-2xl overflow-hidden">
+							<motion.div
+								variants={zoomIn(1.5, 0.75)}
+								className="lg:w-1/2 flex justify-center m-10 rounded-2xl overflow-hidden"
+							>
 								<img
 									src={img1}
 									alt="Sertif"
 									className="w-[432px]"
 								/>
-							</div>
-						</div>
-					</div>
+							</motion.div>
+						</motion.div>
+					</motion.div>
 				</div>
 			</div>
 		</div>

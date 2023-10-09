8c21b715a08a4f0ab15365563c08a5c19a3e3e64
Yunrizal145 <hamad.yunrizal@gmail.com>
2023-10-10 00:18:02 +0700

    style: Menambahkan animasi pada Footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,25 +1,51 @@
 import { identity } from "../constants/data";
 import Icons from "../sections/Icons";
+import { motion } from "framer-motion";
+import { slideIn, staggerContainer, textVariants } from "../utils/motion";
 
 const Footer = () => {
 	const { name, role } = identity;
 
 	return (
-		<footer className="container mx-auto text-white ">
-			<div className="flex flex-col justify-center items-center mt-20">
-				<h2 className="text-xl lg:text-3xl text-center font-bold tracking-widest mb-2">
+		<motion.footer
+			animate={{ y: [350, -50, 0] }}
+			viewport={{ once: true, amount: 0.25 }}
+			transition={{ ease: "easeOut", duration: 2 }}
+			variants={staggerContainer}
+			whileInView="show"
+			initial="hidden"
+			className="container mx-auto text-white "
+		>
+			<motion.div
+				variants={slideIn("bottom", "tween", 0.1, 1)}
+				className="flex flex-col justify-center items-center mt-20"
+			>
+				<motion.h2
+					variants={textVariants(1.1)}
+					className="text-xl lg:text-3xl text-center font-bold tracking-widest mb-2"
+				>
 					{name}
-				</h2>
-				<h4 className="text-lg lg:text-xl">{role[0]} 👨‍💻 </h4>
-
-				<div className="lg:ml-36 lg:-mt-2">
+				</motion.h2>
+				<motion.h4
+					variants={textVariants(1.2)}
+					className="text-lg lg:text-xl"
+				>
+					{role[0]} 👨‍💻{" "}
+				</motion.h4>
+				<motion.div
+					variants={textVariants(1.3)}
+					className="lg:ml-36 lg:-mt-2"
+				>
 					<Icons />
-				</div>
-				<div className="text-xs lg:text-base text-center mt-4 mb-14">
+				</motion.div>
+				<motion.div
+					variants={textVariants(1.4)}
+					className="text-xs lg:text-base text-center mt-4 mb-14"
+				>
 					<div>&copy; 2023 Created by {name}, All Right Deserved</div>
-				</div>
-			</div>
-		</footer>
+				</motion.div>
+			</motion.div>
+		</motion.footer>
 	);
 };
 2;
